ff1a2493b3f885370a5156a3a2610e51b2bcd680
Shivahari Rukesh <478gnujnoej@gmail.com>
2025-06-23 23:20:20 +0545

    Setting up the contract integration

--- frontend/src/pages/Erc20.tsx
+++ frontend/src/pages/Erc20.tsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import ContractAbi from "../contracts/Erc20.json"
+import ContractDetails from "../contracts/deployment.json"
+import { BrowserProvider, Contract, ethers } from 'ethers';
+
 
 declare global {
   interface Window {
@@ -6,9 +10,14 @@ declare global {
   }
 }
 
+const ABI = ContractAbi.abi;
+ 
+
 const Erc20 = () => {
   const [accounts, setAccounts] = useState<string[] | undefined>();
   const [connectedAccount, setConnectedAccount] = useState<string | undefined>();
+  const [provider, setProvider] = useState<BrowserProvider | null>(null);
+const [contract,setContract] = useState<Contract | null>(null)
 
   const checkIfWalletIsConnected = async () => {
     if (!window.ethereum) return;
@@ -38,9 +47,27 @@ const Erc20 = () => {
     };
   }, []);
 
+  useEffect (()=>{
+    const initiateContract =async()=>{
+      if(window.ethereum){
+        const web3Provider = new ethers.BrowserProvider(window.ethereum);
+        setProvider(web3Provider);
+
+        const contractInstance = new ethers.Contract(
+          ContractDetails.address,
+          ABI,
+          web3Provider
+        )
+
+        setContract(contractInstance)
+      }
+    }
+    initiateContract();
+  })
+
   return (
-    <div>
-      <div className='flex flex-row-reverse bg-white text-black'>
+    <div className=' bg-black h-[100vh]'>
+      <div className='flex flex-row-reverse'>
         <nav className='p-6'>
           <ul>
             <li onClick={handleConnectWallet} className='cursor-pointer'>
@@ -56,6 +83,15 @@ const Erc20 = () => {
         ) : (
           <span>Not connected</span>
         )}
+      </div>
+
+      <div className='h-[50%]'>
+        <div className='border-2 border-red-400 w-[30%] h-[50%]'>
+
+        </div>
+        <div>
+
+        </div>
       </div>
     </div>
   );
